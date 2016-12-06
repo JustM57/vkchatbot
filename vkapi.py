1bf4ec8361a702fecb3113dfe64f75bb6b23e0ac
JustM57 <tiir57@yandex.ru>
2016-12-06 16:38:00 +0300

    downloading messages

---
+++ vkapi.py
@@ -0,0 +1,38 @@
+import config as cn
+import vk
+import time
+import sqlite3
+
+session = vk.AuthSession(app_id=cn.vk_client_id, user_login=cn.user_mail, user_password=cn.user_pswd, scope=cn.vk_scope)
+api = vk.API(session, v='5.60')
+nol = 0
+conn = sqlite3.connect('outbox_messages.db')
+c = conn.cursor()
+all_messages = []
+
+for i in range(1644):
+    response = api.messages.get(out=1, offset=i*200, count=200)
+    messages = response['items']
+    print("обработано исходящих ", i*200)
+    for j in range(200):
+        c.execute("INSERT INTO messages (id,in_id,user_id,random_id,message) VALUES (?,?,?,?,?)",(messages[j]['id'],nol,messages[j]['user_id'],messages[j]['random_id'],messages[j]['body']))
+        conn.commit()
+    if (i % 3) == 2:
+        time.sleep(1)
+c.close()
+conn.close()
+conn = sqlite3.connect('imbox_messages.db')
+c = conn.cursor()
+all_messages = []
+
+for i in range(1644):
+    response = api.messages.get(out=0, offset=i*200, count=200)
+    messages = response['items']
+    print("обработано входящих ", i*200)
+    for j in range(200):
+        c.execute("INSERT INTO messages (id,out_id,user_id,random_id,message) VALUES (?,?,?,?,?)",(messages[j]['id'],nol,messages[j]['user_id'],messages[j]['random_id'],messages[j]['body']))
+        conn.commit()
+    if (i % 3) == 2:
+        time.sleep(1)
+c.close()
+conn.close()
